2ed2474377132522889249cb489a728f4e56633e
MJCopper <matt@thecoppers.net>
2026-08-10 21:06:00 +1000

    Make Child Mode policy test runnable

--- test/test_child_mode_policy/test_child_mode_policy.cpp
+++ test/test_child_mode_policy/test_child_mode_policy.cpp
@@ -23,3 +23,8 @@ TEST(ChildModePolicy, BoundsFavouriteChannelMask) {
   EXPECT_FALSE(childmode::favouriteChannelSet(favourites, 1));
   EXPECT_FALSE(childmode::favouriteChannelSet(favourites, 64));
 }
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
